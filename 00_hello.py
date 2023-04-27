2a071793eed1dd937b4d5a1dca369d84311dac37
dst3v3n <hablack10@gmail.com>
2023-04-27 17:30:48 -0500

    1 Dia Aprendiendo Python. Hello Python

---
+++ 00_hello.py
@@ -0,0 +1,30 @@
+#Hola mundo
+print("Hola python")
+print('Hola python')
+
+"""
+Este es un comentario
+en varias lineas
+"""
+
+'''
+Este tambien 
+es un comentario
+en varias lineas
+'''
+
+#Consultar el tipo de dato
+print (type("Soy un dato Str")) #El tipo de dato es "str" (Tipo String o cadena de texto)
+print (type(10)) #El tipo de dato es "int" (Entero)
+print (type(10.5)) #El tipo de dato es "float" (Decimal o flotante)
+print (type(1+3j)) #El tipo de dato es "complex" (numero complejo)
+print (type(True and False)) #El tipo de dato es "bool" (Booleano)
+
+#Operadores numericos
+print(4+5) #Adicion (+)
+print(10-5) #sustraccion (-)
+print(10*5) #Multiplicacion (x)
+print(10/5) #Division (/)
+print(10**5) #Exponenciacion (**)
+print(10%6) #Modulo (%)
+print(10//8) #Division Entera (//)
---
+++ 01_variables.py
@@ -0,0 +1,3 @@
+#Variables
+
+myvariable = "My string variable"
